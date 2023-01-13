140488c319487c597be831ca5044f4b400b0da5d
salah eddine <salimali1768@gmail.com>
2023-01-13 20:16:54 +0100

    added, register, login and home activities

---
+++ app/src/main/java/com/example/internship_management/ActivityHome.java
@@ -0,0 +1,23 @@
+package com.example.internship_management;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ActivityHome extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
